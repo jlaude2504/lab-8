5062912fb1acc4026479c8b5b410894109b5d865
jlaude2504 <jlaude@hawk.iit.edu>
2023-11-07 16:08:27 -0600

    "showMeText" function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function init(){
     var button = document.getElementById('entrybutton');
   
- 
+    function showMeText(){
+      var textbox = document.getElementById('entryinput');
+      document.getElementById('textoutput').innerHTML = textbox.value;
+      alert("Jefferey Jay Laude: " + textbox.value);
+    }
